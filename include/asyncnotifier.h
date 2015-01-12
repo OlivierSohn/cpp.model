ebb363dd42bdc932914149be1a20808dfc091fd9
Olivier Sohn <olivier.sohn@gmail.com>
2015-01-12 09:27:43 +0100

    move / renamed sources

---
+++ include/asyncnotifier.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <list>
+#include <functional>
+
+namespace imajuscule
+{
+    class AsyncNotifier
+    {
+    public:
+        AsyncNotifier(std::function<void(void)> & f) :
+            m_f(f)
+        {}
+        AsyncNotifier(std::function<void(void)> && f) :
+            m_f(f)
+        {}
+        virtual ~AsyncNotifier(){}
+
+        static void runScheduledNotifications();
+        void schedule();
+    protected:
+        std::function<void(void)> m_f;
+    private:
+        typedef std::list<AsyncNotifier*> notifiers; // we use a list because if we use a vector, in runScheduledNotifications if a notification changes the vector, the iterators are not valid anymore!
+        static notifiers g_scheduled;
+        void doNotify();
+    };
+}
---
+++ source/asyncnotifier.cpp
@@ -0,0 +1,37 @@
+#include "asyncnotifier.h"
+#include <cassert>
+
+using namespace imajuscule;
+
+AsyncNotifier::notifiers AsyncNotifier::g_scheduled;
+
+void AsyncNotifier::schedule()
+{
+    g_scheduled.push_back(this);
+}
+
+void AsyncNotifier::runScheduledNotifications()
+{
+    notifiers::iterator it, end;
+
+    it = g_scheduled.begin();
+    end = g_scheduled.end();
+
+    for (; it != end; ++it)
+    {
+        // this call can add elemnts to g_scheduled, it is ok because for a std::list, 
+        // the iterators are not invalidated by insertion (not it nor end)
+        (*it)->doNotify();
+    }
+
+    // assert that end iterator didn't change (even if size of list has changed)
+    assert(end == g_scheduled.end());
+
+    g_scheduled.clear();
+}
+
+
+void AsyncNotifier::doNotify()
+{
+    m_f();
+}
