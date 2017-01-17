2d491e96fe4db366ef01f2409e4186f5e1e21145
Olivier Sohn <olivier.sohn@gmail.com>
2017-01-16 22:55:41 -0500

    support for audio elements in channel

--- include/observable.h
+++ include/observable.h
@@ -8,17 +8,6 @@
 
 namespace imajuscule
 {
-    template <typename E>
-    constexpr typename std::underlying_type<E>::type to_underlying(E e) {
-        return static_cast<typename std::underlying_type<E>::type>(e);
-    }
-    
-    template <typename E>
-    constexpr int min_bits() {
-        return ceil_power_of_two(to_underlying(E::SIZE_ENUM));
-    }
-    
-
     template <typename Event>
     struct FunctionInfo
     {
