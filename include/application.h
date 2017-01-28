1a67c97815a9a7615ecee5d3959f505202878982
Olivier Sohn <olivier.sohn@gmail.com>
2017-01-27 21:24:24 -0500

    less logs

--- include/application.h
+++ include/application.h
@@ -2,15 +2,15 @@
 namespace imajuscule {
     struct Application {
         static void Init() {
-            LG(INFO, "Application::Init ...");
+            //LG(INFO, "Application::Init ...");
             Referentiables::getManagers();
-            LG(INFO, "Application::Init Done");
+            //LG(INFO, "Application::Init Done");
         }
         
         static void TearDown() {
-            LG(INFO, "Application::TearDown ...");
+            //LG(INFO, "Application::TearDown ...");
             Globals::reset();
-            LG(INFO, "Application::TearDown Done");
+            //LG(INFO, "Application::TearDown Done");
         }
     };
     
