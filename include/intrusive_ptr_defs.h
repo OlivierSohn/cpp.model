4b11e1beeafea863c7f2e0606afdba4899c8866f
Olivier Sohn <olivier.sohn@gmail.com>
2017-02-18 20:57:11 +0100

    constexpr modifications, ramps are now better controlled in time

--- include/intrusive_ptr_defs.h
+++ include/intrusive_ptr_defs.h
@@ -1,6 +1,6 @@
 
 namespace imajuscule
 {
-    static constexpr auto intrusive_ptr_zero_count = -1;
+    constexpr auto intrusive_ptr_zero_count = -1;
     // 0 means owned by one only (in order to optimize equality test)
 }
