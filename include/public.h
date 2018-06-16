82c87b0afa4c72f5bc0079a990bf0af3aec20049
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-16 17:11:04 +0200

    adapt

--- include/public.h
+++ include/public.h
@@ -14,7 +14,7 @@
 #include <vector>
 
 #include "../../cpp.algorithms/include/public.h"
-#include "../../cpp.os.logs/include/public.h"
+#include "../../cpp.os.log/include/public.h"
 #include "../../cpp.os.storage/include/public.h"
 
 #include "raii.hpp"
